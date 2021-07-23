2eca4ecdd50fca25bf55dbbb7999d620fa1abb3d
ShadowSol <ShadowSol@mail.com>
2021-07-24 02:37:44 +0300

    Made the banana generator C file.

---
+++ bananagenerator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int		main(void)
+{
+	int		cntr;
+	int		i;
+	8char		*str;
+	char		*m;
+
+	m = "Bananas";
+	str = m;
+	i = 1;
+	printf("Enter amount of bananas: ");
+	scanf(" %d", &cntr);
+	if (cntr <= 0)
+	{
+		printf("No bananas found here.\n");
+		return (1);
+	}
+	while (i <= cntr)
+	{
+		if (i == 1)
+			printf("1 Banana\n");
+		else
+			printf("%d %s \n", i, m);
+		i++;
+	}
+	write(1, "Reject modernity, return to monke\n", 35);
+	return (0);
+}
+
